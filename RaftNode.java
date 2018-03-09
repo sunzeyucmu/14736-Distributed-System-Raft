c82144c1d49af6be677c8d9c69565168499eda71
jhyzwzx <jhyzwzx@163.com>
2018-03-08 23:11:54 -0800

    first commit

--- RaftNode.java
+++ RaftNode.java
@@ -1,13 +1,64 @@
 import lib.*;
+
+import java.rmi.RemoteException;
+
 public class RaftNode implements MessageHandling {
+
+    class LogEntry {
+        // TODO: index necessary ?
+        private int term;
+        private int state;
+        private int index;
+        public LogEntry(int term, int state, int index) {
+            this.state = state;
+            this.term = term;
+            this.index = index;
+        }
+
+        public int getTerm() {
+            return term;
+        }
+
+        public int getState() {
+            return state;
+        }
+    }
+
     private int id;
     private static TransportLib lib;
+    private int port;
     private int num_peers;
+
+    private boolean isLeader = false;
+    private int currentTerm = 0;
+    private int votedFor = -1;   // -1 if no candidate requested for vote yet
+
+    private LogEntry[] logs = {};
+    // Starting from 1, according to the paper
+    private int commitIndex = 0;
+
+    //TODO: Other states
 
     public RaftNode(int port, int id, int num_peers) {
         this.id = id;
         this.num_peers = num_peers;
         lib = new TransportLib(port, id, this);
+        this.port = port;
+    }
+
+    public void requestVote() {
+        currentTerm += 1;
+        byte[] body = BytesUtil.serialize(new RequestVoteArgs(currentTerm, id, commitIndex, logs[commitIndex-1].getTerm()));
+        // TODO: Boradcast by a Loop or sth ; Specify dest_addr
+        Message reqMsg = new Message(MessageType.RequestVoteArgs,  port, 1, body);
+
+        try {
+            Message respMsg = lib.sendMessage(reqMsg);
+            RequestVoteReply reply = (RequestVoteReply) BytesUtil.deserialize(respMsg.getBody());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        // TODO:
     }
 
     /*
